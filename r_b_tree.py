18dce43a600890c57dd5b54502222ddbee1ca441
Lucas Inacio <lucasinacio2003@hotmail.com>
2024-09-20 23:16:00 -0300

    ✨ Initial Commit

---
+++ r_b_tree.py
@@ -0,0 +1,14 @@
+"""
+    Implementação de uma árvore rubro-negra em Python.
+    Feito por Lucas Gomes Inacio.
+    Número USP: 12562091
+    
+    Usando extensão Pylint (ms-python.pylint) no Visual Studio Code.
+    Usando Github Copilot para auxiliar na escrita do código.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
