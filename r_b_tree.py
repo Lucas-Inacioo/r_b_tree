9cb2698749ab9d5f155a18353e8868bc8fd1bfbc
Lucas Inacio <lucasinacio2003@hotmail.com>
2024-09-20 23:24:19 -0300

    ✨ Add README and create Node dataclass

--- r_b_tree.py
+++ r_b_tree.py
@@ -4,11 +4,33 @@
     Número USP: 12562091
     
     Usando extensão Pylint (ms-python.pylint) no Visual Studio Code.
+    https://pylint.readthedocs.io/en/latest/index.html
+    
     Usando Github Copilot para auxiliar na escrita do código.
+
+    Histórico de versões:
+    https://github.com/Lucas-Inacioo/r_b_tree
 """
 
+from __future__ import annotations
+import dataclasses
+
+@dataclasses.dataclass
+class Node:
+    """
+    Classe que representa um nó de uma árvore rubro-negra.
+    Utilizado dataclasses por não precisar de métodos especiais.
+    """
+    key: int
+    color: bool
+    parent: Node
+    left: Node
+    right: Node
+
 def main():
-    pass
+    """
+    Função principal, deve testar a implementação da árvore rubro-negra.
+    """
 
 if __name__ == '__main__':
     main()
